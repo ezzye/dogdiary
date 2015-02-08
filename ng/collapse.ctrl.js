9449a7e470ccddd488e109766ba36d91c506e19f
ezzye <ezzy.elliott@gmail.com>
2015-02-08 18:00:46 +0000

    Add check ofr load

--- ng/collapse.ctrl.js
+++ ng/collapse.ctrl.js
@@ -1,3 +1,6 @@
 angular.module('ui.bootstrap.demo').controller('CollapseCtrl', function ($scope) {
   $scope.isCollapsed = false;
+  $scope.$on('$viewContentLoaded', function(){
+    $scope.isCollapsed =!$scope.isCollapsed;
+  });
 });
