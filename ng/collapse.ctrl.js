ea688f9fe1b8fdb3735a9b0e19d488b3edef9a6d
ezzye <ezzy.elliott@gmail.com>
2015-02-08 18:38:49 +0000

    use module app

--- ng/collapse.ctrl.js
+++ ng/collapse.ctrl.js
@@ -1,4 +1,4 @@
-angular.module('ui.bootstrap').controller('CollapseCtrl', function ($scope) {
+angular.module('app').controller('CollapseCtrl', function ($scope) {
   $scope.isCollapsed = false;
   $scope.$on('$viewContentLoaded', function(){
     $scope.isCollapsed =!$scope.isCollapsed;
