2f40d783769c82be607f2f6eabe04d28e51c3529
ezzye <ezzy.elliott@gmail.com>
2015-02-12 12:59:12 +0000

    Add dogname to user

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -16,7 +16,10 @@ router.get('/', function (req, res, next) {
 })
 
 router.post('/', function (req, res, next) {
-    var user = new User({username: req.body.username})
+    var user = new User({
+        username: req.body.username,
+        dogname: req.body.dogname
+    })
     bcrypt.hash(req.body.password, 10, function (err, hash) {
         if (err) { return next(err) }
         user.password = hash
