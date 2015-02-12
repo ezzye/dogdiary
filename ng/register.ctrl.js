e18200d40fc18a2afad3332aeb9deee827962253
ezzye <ezzy.elliott@gmail.com>
2015-02-12 08:00:05 +0000

    Add dogname to registration controller

--- ng/register.ctrl.js
+++ ng/register.ctrl.js
@@ -1,7 +1,7 @@
 angular.module('app')
 .controller('RegisterCtrl', function ($scope, UserSvc, $location) {
-    $scope.register = function (username, password) {
-        UserSvc.register(username, password)
+    $scope.register = function (username, dogname, password) {
+        UserSvc.register(username, dogname, password)
         .then(function (user) {
         $scope.$emit('login', user)
         $location.path('/')
