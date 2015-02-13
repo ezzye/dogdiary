35a29c77941f1af4f4e1700518495cd4e262199f
ezzye <ezzy.elliott@gmail.com>
2015-02-13 13:04:22 +0000

    Make dogname optional

--- models/post.js
+++ models/post.js
@@ -1,7 +1,7 @@
 var db = require('../db');
 var Post = db.model('Post', {
     username: { type: String, required: true },
-    dogname:  {type: String, required: true },
+    dogname:  {type: String },
     body:     { type: String, required: true },
     date:     { type: Date, required: true, default: Date.now }
 });
