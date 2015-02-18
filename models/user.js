a6a635a44f73e13e1b31714a25e1d5c80984800e
ezzye <ezzy.elliott@gmail.com>
2015-02-18 07:49:33 +0000

    Dog name no longer required

--- models/user.js
+++ models/user.js
@@ -2,6 +2,6 @@ var db = require('../db')
 var user = db.Schema({
     username: { type: String, required: true},
     password: { type: String, required: true, select: false},
-    dogname: {type: String, required: true}
+    dogname: {type: String}
 })
 module.exports = db.model('User', user)
