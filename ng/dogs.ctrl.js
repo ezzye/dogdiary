909bfbe7a064a9f149a894773ae9a73efec5a504
ezzye <ezzy.elliott@gmail.com>
2015-02-19 08:01:26 +0000

    Add post code to start with

--- ng/dogs.ctrl.js
+++ ng/dogs.ctrl.js
@@ -1,1 +1,26 @@
-//code for dog controller
+angular.module('app')
+.controller('PostsCtrl', function($scope,PostsSvc) {
+    $scope.addPost = function () {
+        if($scope.postBody) {
+            PostsSvc.create({
+                body: $scope.postBody,
+                dogname: $scope.currentUser.dogname
+            }).then(function ()
+            {
+                $scope.postBody=null
+            })
+        }
+    }
+
+    $scope.$on('ws:new_post', function (_, post) {
+        $scope.$apply(function () {
+            $scope.posts.unshift(post)
+        })
+    })
+
+    PostsSvc.fetch().success(function (posts) {
+        $scope.posts = posts
+    })
+    
+
+})
