2eeb4ce949789f0d34b34f849a53b39791e477e0
ezzye <ezzy.elliott@gmail.com>
2015-02-19 12:47:44 +0000

    Convert controller to deal with dogs rather than posts

--- ng/dogs.ctrl.js
+++ ng/dogs.ctrl.js
@@ -2,24 +2,24 @@ angular.module('app')
 .controller('DogsCtrl', function($scope,DogsSvc) {
     $scope.addDog = function () {
         if($scope.dogname) {
-            PostsSvc.create({
-                body: $scope.postBody,
-                dogname: $scope.currentUser.dogname
+            DogsSvc.create({
+                dogname: $scope.dogname,
+                ownername: $scope.currentUser.username
             }).then(function ()
             {
-                $scope.postBody=null
+                $scope.dogname=null
             })
         }
     }
 
-    $scope.$on('ws:new_post', function (_, post) {
+    $scope.$on('ws:new_dog', function (_, dog) {
         $scope.$apply(function () {
-            $scope.posts.unshift(post)
+            $scope.dogs.unshift(dog)
         })
     })
 
-    PostsSvc.fetch().success(function (posts) {
-        $scope.posts = posts
+    DogsSvc.fetch().success(function (dogs) {
+        $scope.dogs = dogs
     })
     
 
