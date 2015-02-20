871869d4410e2620440d33326d48361866e32867
ezzye <ezzy.elliott@gmail.com>
2015-02-20 20:41:41 +0000

    Image not required

--- models/dog.js
+++ models/dog.js
@@ -2,6 +2,6 @@ var db = require('../db');
 var Dog = db.model('Dog', {
     ownername: { type: String, required: true },
     dogname:   {type: String,required: true  },
-    image:     { type: String, required: true },
+    image:     { type: String}
 });
 module.exports = Dog;
