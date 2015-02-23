3c5dc09e488e291e6b67e25adce7c1c622921dca
ezzye <ezzy.elliott@gmail.com>
2015-02-23 17:39:32 +0000

    Correct error in log

--- ng/application.ctrl.js
+++ ng/application.ctrl.js
@@ -2,12 +2,12 @@ angular.module('app')
 .controller('ApplicationCtrl', function ($scope) {
     $scope.$on('login', function (_, user) {
         $scope.currentUser = user;
+        console.log(user)    
     })
     
     $scope.$on('dogadded', function (_, dog) {
         $scope.currentDog = dog;
+        console.log(dog)
     })
-    console.log(user)
-    console.log(dog)
 
 })
