3952c81acd4f1915dc323bf20930e52673947d07
ezzye <ezzy.elliott@gmail.com>
2015-02-23 12:34:21 +0000

    Add more logs

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -17,7 +17,8 @@ router.get('/', function (req, res, next) {
 
 router.post('/', function (req, res, next) {
     var user = new User({ username: req.body.username })
-    console.log(req.body.password)
+    console.log("see body below")
+    console.log(req.body)
     bcrypt.hash(req.body.password, 10, function (err, hash) {
         if (err) { 
             console.log("error error error")
