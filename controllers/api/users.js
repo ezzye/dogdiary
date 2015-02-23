ca32eb8756499a8941226de4f7bbeeb2bcb7b709
ezzye <ezzy.elliott@gmail.com>
2015-02-23 11:34:19 +0000

    Remove dogname

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -17,8 +17,7 @@ router.get('/', function (req, res, next) {
 
 router.post('/', function (req, res, next) {
     var user = new User({
-        username: req.body.username,
-        dogname: req.body.dogname
+        username: req.body.username
     })
     bcrypt.hash(req.body.password, 10, function (err, hash) {
         if (err) { return next(err) }
