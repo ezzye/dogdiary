6fde3545fb9b81265ea5c2477a481c812d51492e
ezzye <ezzy.elliott@gmail.com>
2015-02-24 17:34:24 +0000

    add save create example

--- controllers/api/crud.js
+++ controllers/api/crud.js
@@ -16,6 +16,22 @@ db.posts.update({
 })
 
 
+var User = require('mongoose').model('User');
+
+exports.create = function(req, res, next) {
+  var user = new User(req.body);
+
+  user.save(function(err) {
+        if (err) {
+          return next(err);
+        } else {
+          res.json(user);
+        }
+  });
+};
+
+
+
 //Read
 
 db.posts.find()
