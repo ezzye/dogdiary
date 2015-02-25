90d97c99b182cca4357331964aa4fe7a2ef8be12
ezzye <ezzy.elliott@gmail.com>
2015-02-25 21:34:56 +0000

    Add current dog

--- ng/application.ctrl.js
+++ ng/application.ctrl.js
@@ -3,5 +3,8 @@ angular.module('app')
     $scope.$on('login', function (_, user) {
         $scope.currentUser = user;
     })
+    $scope.$on('addDog', function (_, dog) {
+        $scope.currentdog = dog;
+    })
 
 })
