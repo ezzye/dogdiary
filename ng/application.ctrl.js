06c7ce2a6468e16f36a218156889dfc942264145
ezzye <ezzy.elliott@gmail.com>
2015-02-26 07:58:06 +0000

    Move logs

--- ng/application.ctrl.js
+++ ng/application.ctrl.js
@@ -1,13 +1,13 @@
 angular.module('app')
 .controller('ApplicationCtrl', function ($scope) {
     $scope.$on('login', function (_, user) {
-        $scope.currentUser = user;
+        $scope.currentUser = user
     })
     $scope.$on('addDog', function (_, dog) {
-        $scope.currentDog = dog;
+        $scope.currentDog = dog
+        console.log("This is me emitting dog")
+        console.log($scope.currentDog)
+        console.log($scope.currentDog.ownername)
+        console.log($scope.currentDog.dogname)
     })
-    console.log("This is me emitting dog")
-    console.log($scope.currentDog)
-    console.log($scope.currentDog.ownername)
-    console.log($scope.currentDog.dogname)
 })
