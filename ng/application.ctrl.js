62131f8ebb2588aafa6ede5d92a63f17c4990a63
ezzye <ezzy.elliott@gmail.com>
2015-03-03 19:15:53 +0000

    Add dogname

--- ng/application.ctrl.js
+++ ng/application.ctrl.js
@@ -5,7 +5,7 @@ angular.module('app')
     })
     $scope.$on('addDog', function (_, dog) {
         $scope.currentDog = dog
-        console.log("This is the dog",$scope.currentDog)
+        console.log("This is the dog",$scope.currentDog.dogname)
         console.log("This is the dog object", dog)
     })
 })
