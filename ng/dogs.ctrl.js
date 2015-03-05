a03ef2cddf05715bf83b5baa1bbd6da273b02532
ezzye <ezzy.elliott@gmail.com>
2015-03-05 08:03:16 +0000

    Remove logs

--- ng/dogs.ctrl.js
+++ ng/dogs.ctrl.js
@@ -5,10 +5,6 @@ angular.module('app')
                 dogname: $scope.dogname,
                 ownername: $scope.currentUser.username
             }).then(function (_, dog) {
-                console.log('This is the scope .dogname', $scope.dogname)
-                console.log('This is the scope .dog', $scope.dog)
-                console.log('This is the scope .dogs', $scope.dogs)
-                console.log('This is the scope .dog.dogname', $scope.dog.dogname)
                 $scope.emit('dogcreate',$scope.dogname)
                 $location.path('/')
             })
