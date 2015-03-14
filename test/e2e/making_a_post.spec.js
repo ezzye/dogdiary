1966546ba44b6b1f6f798d79f8ffddd63f5c938e
ezzye <ezzy.elliott@gmail.com>
2015-03-14 19:27:30 +0000

    add browser pause

--- test/e2e/making_a_post.spec.js
+++ test/e2e/making_a_post.spec.js
@@ -3,24 +3,27 @@ chai.use(require('chai-as-promised'))
 var expect = require('chai').expect
 
 
-describe('making a post', function () {
-    it('creates an account and a new post', function () {
+describe('making a post', function() {
+    it('creates an account and a new post', function() {
+        // launch app
         browser.get('http://localhost:3001')
+        
         // click 'login'
         element(by.css('nav .login')).click()
-        
+        browser.pause()
+
         // fill out and submit login form
         element(by.model('username')).sendKeys('dickeyxxx')
         element(by.model('password')).sendKeys('pass')
         element(by.css('form .btn')).click()
 
         // submit a new post on the posts page
-        var post = 'my new post' + Math.random ()
+        var post = 'my new post' + Math.random()
         element(by.model('postBody')).sendKeys(post)
         element(by.css('form .btn')).click()
-        
+
         // the user should now see their post as the first post on the page
         expect(element.all(by.css('ul.list-group li')).first()
-        .getText()).to.eventually.contain(post)
+            .getText()).to.eventually.contain(post)
     })
 })
